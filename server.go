15d63cae83f5954a187b830b564a16a44388af09
Professor108 <panstepan13@i.ua>
2021-10-04 00:35:24 +0300

    Add time displaying

--- server.go
+++ server.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+	"encoding/json"
 )
 
 func feature() {
@@ -13,8 +15,20 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Server is running on Go...")
 	})
+
 	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Now time is:")
+		tm := time.Now()
+		response := fmt.Sprintf("Now time is: %d-%02d-%02dT%02d:%02d:%02d-00:00\n",
+			tm.Year(), tm.Month(), tm.Day(),
+			tm.Hour(), tm.Minute(), tm.Second())
+		fmt.Fprintf(w, response)
+		jsonData, err := json.Marshal(TimeS{Time: tm.Format(time.RFC3339)})
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(jsonData))
+		fmt.Fprintf(w, string(jsonData))
 	})
+
 	http.ListenAndServe(":8795", nil)
 }
